dda087471c4486135862cb7f046912b094c32861
venkatesh02040 <ysvh.pandu@gmail.com>
2025-03-08 00:26:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const jsonServer = require("json-server"); // importing json-server library
+const jsonServer = require("json-server");
+const cors = require("cors"); // Import CORS middleware
+
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
+const port = process.env.PORT || 8080; // Choose port like 8080, 3001
 
+server.use(cors()); 
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+server.listen(port)
